96aa771ad548820f2ec2c261faa9b1edf24209f2
-tariromashanda <tariromashanda@proton.me>
2025-08-19 09:21:27 +0200

    create buyer class

---
+++ src/main/java/marketplace/Buyer.java
@@ -0,0 +1,7 @@
+package marketplace;
+
+public class Buyer extends User{
+    public Buyer(String name, String surname, String email, String business) {
+        super(name, surname, email, business);
+    }
+}
