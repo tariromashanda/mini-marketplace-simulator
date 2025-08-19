25a04eb6dfc6335028c85b65d330689ca652b760
-tariromashanda <tariromashanda@proton.me>
2025-08-19 09:02:44 +0200

    create Seller Class

---
+++ src/main/java/marketplace/Seller.java
@@ -0,0 +1,20 @@
+package marketplace;
+
+public class Seller {
+
+
+    private String name;
+    private String surname;
+    private String email;
+    private String business;
+
+
+    public Seller(String name, String surname, String email, String business){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.business = business;
+
+
+    }
+}
