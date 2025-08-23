a1bdbbcc5ceebbc39ad4570f46e4c80b73887b2a
-tariromashanda <tariromashanda@proton.me>
2025-08-23 15:14:13 +0200

    update buyer/seller logic and remove register class

--- src/main/java/marketplace/Seller.java
+++ src/main/java/marketplace/Seller.java
@@ -1,7 +1,7 @@
 package marketplace;
 
 public class Seller extends User{
-    public Seller(String name, String surname, String email, String username){
+    public Seller(String name, String surname, String email, String username, String password){
         super(name, surname, email, username, password);
     }
 }
