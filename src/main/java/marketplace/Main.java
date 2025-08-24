1f9c08b4b9b34eb1fd2c8d0d243d87e105760310
-tariromashanda <tariromashanda@proton.me>
2025-08-24 06:43:48 +0200

    add user and buyer dashboard

--- src/main/java/marketplace/Main.java
+++ src/main/java/marketplace/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String input;
         int option;
 
         Scanner scanner = new Scanner(System.in);
@@ -61,9 +60,6 @@ public class Main {
                     System.out.println(newSeller);
 
                 }
-
-
-
 
             }
 
---
+++ src/main/java/marketplace/SellerDashboard.java
@@ -0,0 +1,11 @@
+package marketplace;
+
+public class SellerDashboard {
+    public static void Dashboard(){
+        System.out.println("1. Add Item");
+        System.out.println("2. Edit Item");
+        System.out.println("3. Delete item");
+        System.out.println("4. View My Listings");
+        System.out.println("5. Logout");
+    }
+}
