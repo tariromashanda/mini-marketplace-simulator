820b5f055d82796ce74222bf99616c104a2be1fc
-tariromashanda <tariromashanda@proton.me>
2025-08-24 07:08:11 +0200

    create Item class

---
+++ src/main/java/marketplace/Item.java
@@ -0,0 +1,18 @@
+package marketplace;
+
+public class Item {
+    private String name;
+    private String price;
+    private String category;
+    private String description;
+
+    public Item(String name, String price, String category, String description){
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+    }
+
+
+
+}
--- src/main/java/marketplace/Seller.java
+++ src/main/java/marketplace/Seller.java
@@ -1,9 +1,14 @@
 package marketplace;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seller extends User{
     public Seller(String name, String surname, String email, String username, String password){
         super(name, surname, email, username, password);
     }
+
+
 
 
 }
