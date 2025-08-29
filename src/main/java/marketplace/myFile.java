bd1dc976ecc9a87cb4c7d404cee0c7ca6a3b95e9
-tariromashanda <tariromashanda@proton.me>
2025-08-29 11:43:32 +0200

    add readInData

--- src/main/java/marketplace/myFile.java
+++ src/main/java/marketplace/myFile.java
@@ -1,10 +1,10 @@
 package marketplace;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class myFile {
     //writing seller to the file
@@ -33,5 +33,27 @@ public class myFile {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static Map<String, List<String>> readInData(String filepath){
+        Map <String, List<String>> map = new HashMap<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String line = null;
+            while((line.equals(reader.readLine()))){
+                String[] parts = line.split(" ",2);
+                    String username = parts[0].trim();
+                    String attributes = parts[1].trim();
+                    map.put(username, Collections.singletonList(attributes));
+
+
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return map;
     }
 }
