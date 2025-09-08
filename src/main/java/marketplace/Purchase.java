3300311cfce9a27fb4ae0876e1207c10bad299f6
-tariromashanda <tariromashanda@proton.me>
2025-09-08 20:54:20 +0200

    purchase class completed

--- src/main/java/marketplace/Purchase.java
+++ src/main/java/marketplace/Purchase.java
@@ -1,10 +1,26 @@
 package marketplace;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Purchase {
-    List<Transaction> purchase = new ArrayList<>();
+    private String buyer;
+    private String seller;
+    private Item item;
 
+    public Purchase(String buyer, String seller, Item item){
+        this.buyer = buyer;
+        this.seller = seller;
+        this.item = item;
 
+    }
+
+    public String getBuyer() {
+        return this.buyer;
+    }
+
+    public String getSeller() {
+        return this.seller;
+    }
+
+    public Item getItem() {
+        return this.item;
+    }
 }
---
+++ src/main/java/marketplace/Transactions.java
@@ -0,0 +1,27 @@
+package marketplace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transactions {
+    List<Purchase> transactions = new ArrayList<>();
+
+    public void add(Purchase purchase){
+        transactions.add(purchase);
+    }
+
+    public List<Purchase> getTransactions() {
+        return transactions;
+    }
+
+    public void view(){
+        for (Purchase purchase : transactions) {
+            System.out.println(purchase.getBuyer());
+            System.out.println(purchase.getSeller());
+            System.out.println(purchase.getItem());
+            System.out.println();
+        }
+    }
+
+
+}
